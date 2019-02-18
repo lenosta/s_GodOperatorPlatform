3f95b6776c8a23b72e80528c4c73b9898f98c835
zhangtao <zhangtao@163.com>
2019-02-18 19:19:46 +0800

    first

---
+++ GodOperatorPlatform/js/Popover requires tooltip.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" type="image/png">
+  <title>神算子平台</title>
+<link rel="shortcut icon" href="/favicon.png"><link href="/app_4ee4b0ca589423f34e48.css" rel="stylesheet"></head>
+<body>
+<div id="jusfoun"></div>
+<script type="text/javascript" src="/vendor.js"></script><script type="text/javascript" src="/app_4ee4b0ca589423f34e48.js"></script></body>
+</html>
